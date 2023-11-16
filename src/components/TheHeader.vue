cd488cbe31fab4ea4ba7ab24dfe00ce5819e242e
Régis Picáz <registavares@protonmail.com>
2023-11-16 02:01:05 -0300

    FirstCommit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Hello header</h1>
+  <p>This is a header with Vue.Js</p>
+</template>
+
+<script>
+
+export default {
+  name: 'TheHeader',
+  props: {
+    msg: String
+  }
+}
+
+</script>
+
+<style>
+
+h1 {
+  color: #42b983;
+}
+
+</style>
